2b312ca33c0c357616cb2b578ba4df7956bd5a48
Sanjana679 <sanjana@nandi.net>
2019-08-17 17:21:34 -0700

    Bug not fixed yet

--- Exploration.py
+++ Exploration.py
@@ -12,3 +12,38 @@ count_matrix = count.fit_transform(movies['important_words'])
 
 
 cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+indices = pd.Series(GatherAndCleanData.getMovies().index)
+indices[:5]
+
+# function that takes in movie title as input and returns the top 10 recommended movies
+def recommendations(title, cosine_sim = cosine_sim):
+    
+    recommended_movies = []
+    
+    # gettin the index of the movie that matches the title
+    count = 0
+    while count < len(movies['title']):
+        selected_movie_index = 0
+
+        if movies['title'][count] == title:
+            selected_movie_index = count
+        count = count + 1
+
+    # creating a Series with the similarity scores in descending order
+    score_series = pd.Series(cosine_sim[selected_movie_index]).sort_values(ascending = False)
+
+    # getting the indexes of the 10 most similar movies
+    top_10_indexes = list()
+    
+    for i in range(10):
+        top_10_indexes.append(i)
+
+    # populating the list with the titles of the best 10 matching movies
+    for i in top_10_indexes:
+        recommended_movies.append(movies['title'][i])
+        
+    return recommended_movies
+
+
+print(recommendations('The Lone Ranger'))
--- GatherAndCleanData.py
+++ GatherAndCleanData.py
@@ -4,36 +4,34 @@ from rake_nltk import Rake
 import string
 
 # Cleans the string for random punctuation, numbers, and quotations
-def clean_1 (string):
+# test = true when it is the genre column 
+def clean_1 (string, test):
     string = string.replace('[', '', -1)
     string = string.replace(']', '', -1)
     string = string.replace('{', '', -1)
     string = string.replace('}', '', -1)
     string = string.replace('"', '', -1)
-    string = string.replace('name', '', -1)
-    string = string.replace('id', '', -1)
-    string = string.replace('1', '', -1)
-    string = string.replace('2', '', -1)
-    string = string.replace('3', '', -1)
-    string = string.replace('4', '', -1)
-    string = string.replace('5', '', -1)
-    string = string.replace('6', '', -1)
-    string = string.replace('7', '', -1)
-    string = string.replace('8', '', -1)
-    string = string.replace('9', '', -1)
-    string = string.replace('0', '', -1)
+
+    if(test == True):
+        string = string.replace('name', '', -1)
+        string = string.replace('id', '', -1)
+        string = string.replace('1', '', -1)
+        string = string.replace('2', '', -1)
+        string = string.replace('3', '', -1)
+        string = string.replace('4', '', -1)
+        string = string.replace('5', '', -1)
+        string = string.replace('6', '', -1)
+        string = string.replace('7', '', -1)
+        string = string.replace('8', '', -1)
+        string = string.replace('9', '', -1)
+        string = string.replace('0', '', -1)
     string = string.replace(':', '', -1)
-    string = string.replace('\'\'', ',', -1)
     string = string.replace('(', '', -1)
     string = string.replace(')', '', -1)
+    string = string.replace('\'\'', ', ', -1)
 
-    while string.find(',,') != -1:
-        string = string.replace(',,', ',', -1)
-    
     while string.find(', ,') != -1:
         string = string.replace(', ,', ',', -1)
-
-    string = string.replace('\'\'', ', ', -1)
 
     while string.find('\'') != -1:
         string = string.replace('\'', '', -1)
@@ -43,14 +41,15 @@ def clean_1 (string):
 
     while string.find(',,') != -1:
         string = string.replace(',,', ',', -1)
-
+    
+    string = string.replace(' ', '', -1)
     return string
 
 #extracts the keywords from the from_column, and puts it in the to_column
-def extract_keywords(from_column, to_column):
+def extract_keywords(from_column, to_column, test):
     for index, row in movies.iterrows():
         holder = str(row[from_column])
-        
+
         # instantiating Rake, by default it uses english stopwords from NLTK
         # and discards all puntuation characters as well
         r = Rake()
@@ -62,19 +61,31 @@ def extract_keywords(from_column, to_column):
         key_words_dict_scores = r.get_word_degrees()
         
         # assigning the key words to the new column for the corresponding movie
-        movies[to_column][index] = movies[to_column][index] + clean_1(str(list(key_words_dict_scores.keys())))
+
+        if(test == True):
+            movies[to_column][index] = movies[to_column][index] + clean_1(str(list(key_words_dict_scores.keys())), True) + ' '
+        else:   
+            movies[to_column][index] = movies[to_column][index] + clean_1(str(list(key_words_dict_scores.keys())), False) + ' '
     return
 
+def put_in_important_words(from_column):
+    for index, row in movies.iterrows():
+        holder = str(row[from_column])
+        holder = holder.replace(' ', '', -1)
+        movies['important_words'][index] = movies['important_words'][index]+ holder + ' '
+    return
 #########################################################################
 
 movies = pd.read_csv("tmdb_5000_movies.csv")
 
-movies = movies[['genres', 'overview', 'vote_average']]
+movies = movies[['title', 'genres', 'overview', 'vote_average']]
 movies['important_words'] = ''
 
 #extract the keywords from each column and put the keywords in the important_words column 
-extract_keywords('overview', 'important_words')
-extract_keywords('genres', 'important_words')
+put_in_important_words('title')
+extract_keywords('overview', 'important_words', False)
+extract_keywords('genres', 'important_words', True)
+put_in_important_words('title')
 
 #after extracting the keywords, remove the columns except for important_words and vote_average
 movies.drop(columns = ['overview'], inplace = True)
@@ -87,6 +98,6 @@ while count < len(movies['important_words']):
     movies['important_words'][count] = movies['important_words'][count].replace(',',' ', -1)
     count = count + 1
 
-
+#print(movies['important_words'][61])
 def getMovies():
     return movies
